d367b2d9f43ff77de795b60f278cf3ae3052585d
Kostyan <78592281+unordered-set@users.noreply.github.com>
2024-01-27 20:50:32 +0000

    Update Tokenomics.jsx

--- beer/src/components/tokenomics/Tokenomics.jsx
+++ beer/src/components/tokenomics/Tokenomics.jsx
@@ -10,6 +10,13 @@ function Tokenomics (){
                     <div className="token__table-text"><p id='p1'>Tax</p><p>0</p></div>
                     <div className="token__table-text"><p id='p1'>Liquidity</p> <p>Burned Forever</p></div>
                 </div>
+                <div className="token__table-subtitle"><p id='p1'>Initial Liquidity Distribution</div>
+                <div className="token__table">
+                    <div className="token__table-text"><p id='p1'>90% &nbsp; Liquidity Pool</div>
+                    <div className="token__table-text"><p id='p1'>5% &nbsp; Team</div>
+                    <div className="token__table-text"><p id='p1'>3% &nbsp; Airdrops</div>
+                    <div className="token__table-text"><p id='p1'>3% &nbsp; Marketing</div>
+                </div>
                 <div className="token__table-subtitle"><p id='p1'>Token Address </p><p>Burned Forever</p></div>
                 <div className="token__btn"><button>Buy $BEER</button></div>    
 
