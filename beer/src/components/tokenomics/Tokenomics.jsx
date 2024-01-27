283fe00d29b258a251420bcfad8c70f20e610ba8
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-01-27 21:56:25 +0000

    some fix

--- beer/src/components/tokenomics/Tokenomics.jsx
+++ beer/src/components/tokenomics/Tokenomics.jsx
@@ -10,12 +10,12 @@ function Tokenomics (){
                     <div className="token__table-text"><p id='p1'>Tax</p><p>0</p></div>
                     <div className="token__table-text"><p id='p1'>Liquidity</p> <p>Burned Forever</p></div>
                 </div>
-                <div className="token__table-subtitle"><p id='p1'>Initial Liquidity Distribution</div>
+                <div className="token__table-subtitle"><p id='p1'>Initial Liquidity Distribution</p></div>
                 <div className="token__table">
-                    <div className="token__table-text"><p id='p1'>90% &nbsp; Liquidity Pool</div>
-                    <div className="token__table-text"><p id='p1'>5% &nbsp; Team</div>
-                    <div className="token__table-text"><p id='p1'>3% &nbsp; Airdrops</div>
-                    <div className="token__table-text"><p id='p1'>2% &nbsp; Marketing</div>
+                    <div className="token__table-text"><p id='p1'>90% &nbsp; Liquidity Pool</p></div>
+                    <div className="token__table-text"><p id='p1'>3% &nbsp; Team</p></div>
+                    <div className="token__table-text"><p id='p1'>3% &nbsp; Airdrops</p></div>
+                    <div className="token__table-text"><p id='p1'>4% &nbsp; Marketing</p></div>
                 </div>
                 <div className="token__table-subtitle"><p id='p1'>Token Address </p><a href='https://solscan.io/token/BEERx3e69rUB6vkqdMCHPJZRP1FrffsfgZZ41fCRqZDs'><p id='tadress'>BEERx3e69rUB6vkqdMCHPJZRP1FrffsfgZZ41fCRqZDs</p></a></div>
                 <div className="token__btn"><a href='https://raydium.io/swap/?inputCurrency=sol&outputCurrency=BEERx3e69rUB6vkqdMCHPJZRP1FrffsfgZZ41fCRqZDs&fixed=in'><button>Buy $BEER</button></a></div>    
